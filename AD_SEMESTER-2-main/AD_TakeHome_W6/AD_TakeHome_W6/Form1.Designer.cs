c577517226aa010afc77c22e9f17a7d8b38e8afd
WIlliam Kesuma <127371994+WilliamKesuma@users.noreply.github.com>
2023-04-06 00:57:04 +0700

    Add files via upload

---
+++ AD_SEMESTER-2-main/AD_TakeHome_W6/AD_TakeHome_W6/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AD_TakeHome_W6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_Input = new System.Windows.Forms.TextBox();
+            this.BTN_Play = new System.Windows.Forms.Button();
+            this.WORDLE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TB_Input
+            // 
+            this.TB_Input.Location = new System.Drawing.Point(133, 96);
+            this.TB_Input.Name = "TB_Input";
+            this.TB_Input.Size = new System.Drawing.Size(112, 20);
+            this.TB_Input.TabIndex = 0;
+            // 
+            // BTN_Play
+            // 
+            this.BTN_Play.Location = new System.Drawing.Point(158, 140);
+            this.BTN_Play.Name = "BTN_Play";
+            this.BTN_Play.Size = new System.Drawing.Size(65, 23);
+            this.BTN_Play.TabIndex = 1;
+            this.BTN_Play.Text = "Play";
+            this.BTN_Play.UseVisualStyleBackColor = true;
+            this.BTN_Play.Click += new System.EventHandler(this.BTN_Play_Click);
+            // 
+            // WORDLE
+            // 
+            this.WORDLE.AutoSize = true;
+            this.WORDLE.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WORDLE.Location = new System.Drawing.Point(111, 33);
+            this.WORDLE.Name = "WORDLE";
+            this.WORDLE.Size = new System.Drawing.Size(157, 37);
+            this.WORDLE.TabIndex = 2;
+            this.WORDLE.Text = "WORDLE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 244);
+            this.Controls.Add(this.WORDLE);
+            this.Controls.Add(this.BTN_Play);
+            this.Controls.Add(this.TB_Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_Input;
+        private System.Windows.Forms.Button BTN_Play;
+        private System.Windows.Forms.Label WORDLE;
+    }
+}
+
---
+++ AD_SEMESTER-2-main/AD_TakeHome_W6/AD_TakeHome_W6/Form2.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace AD_TakeHome_W6
+{
+    public partial class Form2 : Form
+    {
+        string answer;
+        string guess = "";
+        int line;
+        int transfer = 0;
+        int location1 = 50;
+        int location2 = 50;
+        public Form2(int random)
+        {
+            InitializeComponent();
+            transfer = random;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < transfer; j++)
+                {
+                    Button tombol = new Button();
+                    tombol.Size = new Size(50, 50);
+                    tombol.Location = new Point(location1 + 50 * i, location2 + 50 * j) ;
+                    tombol.Tag = j.ToString() + "," + i.ToString();
+                    tombol.Click += new EventHandler(Button_Click);
+                    this.Controls.Add(tombol);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button anyname = sender as Button;
+            MessageBox.Show(anyname.Tag.ToString());
+        }
+
+        private void createanswer()
+        {
+            answer = "APPLE";
+        }
+
+        private void Update(string keypress)
+        {
+            if (keypress == "ENTER")
+            {
+
+            }
+            else if (keypress == "DELETE")
+            {
+                DeleteButton(guess.Length);
+            }
+            else
+            {
+                if(guess.Length == 5)
+                {
+
+                }
+                else
+                {
+                  guess = guess + keypress;
+                }
+                UpdateButtonPannel(guess.Length);
+            }
+        }
+
+        private void UpdateButtonPannel(int keylength)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                foreach(Button button in this.Controls.OfType <Button> ())
+                {
+                    string tag = button.Tag.ToString();
+                    if (tag == line.ToString() + "," + i.ToString() && i < keylength)
+                    {
+                        button.Text = guess[i].ToString();
+                    }
+                    else if(tag == line.ToString() + "," + i.ToString())
+                    {
+                        button.Text = "";
+                    }
+                }   
+            } 
+        }
+        
+        private void DeleteButton(int keylength)
+        {
+            guess = guess.Substring(0, guess.Length - 1);
+            UpdateButtonPannel(guess.Length);
+        }
+
+
+        private void BTN_Q_Click(object sender, EventArgs e)
+        {
+            Update("Q");
+        }
+
+        private void BTN_W_Click(object sender, EventArgs e)
+        {
+            Update("W");
+        }
+
+        private void BTN_E_Click(object sender, EventArgs e)
+        {
+            Update("E");
+        }
+
+        private void BTN_R_Click(object sender, EventArgs e)
+        {
+            Update("R");
+        }
+
+        private void BTN_T_Click(object sender, EventArgs e)
+        {
+            Update("T");
+        }
+
+        private void BTN_Y_Click(object sender, EventArgs e)
+        {
+            Update("Y");
+        }
+
+        private void BTN_U_Click(object sender, EventArgs e)
+        {
+            Update("U");
+        }
+
+        private void BTN_I_Click(object sender, EventArgs e)
+        {
+            Update("I");
+        }
+
+        private void BTN_O_Click(object sender, EventArgs e)
+        {
+            Update("O");
+        }
+
+        private void BTN_P_Click(object sender, EventArgs e)
+        {
+            Update("P");
+        }
+
+        private void BTN_A_Click(object sender, EventArgs e)
+        {
+            Update("A");
+        }
+
+        private void BTN_S_Click(object sender, EventArgs e)
+        {
+            Update("S");
+        }
+
+        private void BTN_D_Click(object sender, EventArgs e)
+        {
+            Update("D");
+        }
+
+        private void BTN_F_Click(object sender, EventArgs e)
+        {
+            Update("F");
+        }
+
+        private void BTN_G_Click(object sender, EventArgs e)
+        {
+            Update("G");
+        }
+
+        private void BTN_H_Click(object sender, EventArgs e)
+        {
+            Update("H");
+        }
+
+        private void BTN_J_Click(object sender, EventArgs e)
+        {
+            Update("J");
+        }
+
+        private void BTN_K_Click(object sender, EventArgs e)
+        {
+            Update("K");
+        }
+
+        private void BTN_L_Click(object sender, EventArgs e)
+        {
+            Update("L");
+        }
+
+        private void BTN_Z_Click(object sender, EventArgs e)
+        {
+            Update("Z");
+        }
+
+        private void BTN_X_Click(object sender, EventArgs e)
+        {
+            Update("X");
+        }
+
+        private void BTN_C_Click(object sender, EventArgs e)
+        {
+            Update("C");
+        }
+
+        private void BTN_V_Click(object sender, EventArgs e)
+        {
+            Update("V");
+        }
+
+        private void BTN_B_Click(object sender, EventArgs e)
+        {
+            Update("B");
+        }
+
+        private void BTN_N_Click(object sender, EventArgs e)
+        {
+            Update("N");
+        }
+
+        private void BTN_M_Click(object sender, EventArgs e)
+        {
+            Update("M");
+        }
+
+        private void BTN_Delete_Click(object sender, EventArgs e)
+        {
+            Update("DELETE");
+        }
+
+        private void BTN_Enter_Click(object sender, EventArgs e)
+        {
+            Update("ENTER");
+        }
+    }
+}
